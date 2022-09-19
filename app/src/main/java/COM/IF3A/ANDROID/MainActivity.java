a289707a30d2b3bef39138b6e168ff70341dd1d2
brodytiktok <kevinpratama161000@gmail.com>
2022-09-19 16:51:44 +0700

    MyApplication

---
+++ app/src/main/java/COM/IF3A/ANDROID/MainActivity.java
@@ -0,0 +1,27 @@
+package COM.IF3A.ANDROID;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnDaftar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnDaftar = findViewById(R.id.btn_daftar);
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view ) {
+                Toast.makeText(MainActivity.this, "Anda Sudah terdaftar !", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
